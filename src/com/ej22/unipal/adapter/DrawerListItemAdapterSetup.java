d1f42bf0577a82cba82b6e6daa04a0768d8f1e8e
Stephen Hanley <steoh@hotmail.co.uk>
2014-04-11 19:13:25 +0100

    Finished drawer_list_custom_layout file and DrawerListItemAdapterSetup

--- src/com/ej22/unipal/adapter/DrawerListItemAdapterSetup.java
+++ src/com/ej22/unipal/adapter/DrawerListItemAdapterSetup.java
@@ -4,38 +4,59 @@ import java.util.ArrayList;
 
 import com.ej22.unipal.model.DrawerListItemSetup;
 
+import android.R;
+import android.app.Activity;
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DrawerListItemAdapterSetup extends BaseAdapter {
 
 	private Context context;
 	private ArrayList<DrawerListItemSetup> drawerListItem;
 	
+	public DrawerListItemAdapterSetup(Context context, ArrayList<DrawerListItemSetup> drawerListItem){
+		this.context = context;
+		this.drawerListItem = drawerListItem;
+	}
+
+	@Override
+	public Object getItem(int pos) {
+		// TODO Auto-generated method stub
+		return drawerListItem.get(pos);
+	}
+
+	@Override
+	public long getItemId(int pos) {
+		// TODO Auto-generated method stub
+		return pos;
+	}
+
+	@Override
+	public View getView(int pos, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		if (convertView == null){
+			LayoutInflater mInflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+			convertView = mInflater.inflate(com.ej22.unipal.R.layout.drawer_list_custom_layout, null);
+		}
+		
+		ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+		TextView title = (TextView)convertView.findViewById(R.id.title);
+		
+		imgIcon.setImageResource(drawerListItem.get(pos).getIcon());
+		title.setText(drawerListItem.get(pos).getTitle());
+		
+		return convertView;
+	}
+
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
 		return 0;
-	}
-
-	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
