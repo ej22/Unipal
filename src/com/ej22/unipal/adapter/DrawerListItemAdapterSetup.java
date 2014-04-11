88708c8d065ce68c4f019286503f58afe6bc19cc
Stephen Hanley <steoh@hotmail.co.uk>
2014-04-11 13:54:21 +0100

    Added book_icon.png as placeholder. Added drawer layout, model class and implemented start of custom adapter.

---
+++ src/com/ej22/unipal/adapter/DrawerListItemAdapterSetup.java
@@ -0,0 +1,41 @@
+package com.ej22.unipal.adapter;
+
+import java.util.ArrayList;
+
+import com.ej22.unipal.model.DrawerListItemSetup;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class DrawerListItemAdapterSetup extends BaseAdapter {
+
+	private Context context;
+	private ArrayList<DrawerListItemSetup> drawerListItem;
+	
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/ej22/unipal/model/DrawerListItemSetup.java
@@ -0,0 +1,30 @@
+package com.ej22.unipal.model;
+
+public class DrawerListItemSetup {
+
+	private String title;
+	private int icon;
+	
+	public DrawerListItemSetup(){}
+	
+	public DrawerListItemSetup(String title, int icon){
+		this.title = title;
+		this.icon = icon;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public int getIcon(){
+		return icon;
+	}
+	
+	public void setTitle(String title){
+		this.title=title;
+	}
+	
+	public void setIcon(int icon){
+		this.icon = icon;
+	}
+}
