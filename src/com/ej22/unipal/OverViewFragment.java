acb49a6045467c00f2d5550264bb5b572673ff4d
Stephen Hanley <steoh@hotmail.co.uk>
2014-04-14 21:55:26 +0100

    colorpicker test implemented

--- src/com/ej22/unipal/OverViewFragment.java
+++ src/com/ej22/unipal/OverViewFragment.java
@@ -2,9 +2,12 @@ package com.ej22.unipal;
 
 import java.util.Calendar;
 
+import com.android.colorpicker.*;
+
 import android.app.DatePickerDialog;
 import android.app.DatePickerDialog.OnDateSetListener;
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +16,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.Toast;
+import android.util.*;
 
 public class OverViewFragment extends Fragment {
 	
 	public OverViewFragment(){};
 	
 	Button btn;
+	
+	int[] mColor;
 	
 	private int day, month, year;
 	
@@ -29,30 +35,32 @@ public class OverViewFragment extends Fragment {
 		month = c.get(Calendar.MONTH);
 		year = c.get(Calendar.YEAR);
 		
+		mColor = Utils.ColorUtils.colorChoice(getActivity());
+		
 		View rootView = inflater.inflate(R.layout.fragment_overview, container, false);
 		btn = (Button)rootView.findViewById(R.id.btn);
+		
+		final ColorPickerDialog cpg = ColorPickerDialog.newInstance(
+	              R.string.color_picker_default_title, mColor, 0, 5,
+	              Utils.isTablet(getActivity())? ColorPickerDialog.SIZE_LARGE : ColorPickerDialog.SIZE_SMALL);
+		
+		cpg.setOnColorSelectedListener(new ColorPickerSwatch.OnColorSelectedListener() {
+			
+			@Override
+			public void onColorSelected(int color) {
+				// TODO Auto-generated method stub
+				Toast.makeText(getActivity(), "COLOR: " + color, Toast.LENGTH_SHORT).show();
+			}
+		});
 		
 		btn.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View arg0) {
-				DatePickerDialog dpg = new DatePickerDialog(getActivity(), new OnDateSetListener(){
-
-					@Override
-					public void onDateSet(DatePicker picker, int selectedYear, int selectedMonth,
-							int selectedDay) {
-						
-						Toast.makeText(getActivity(), "Date Set at: " + selectedDay + "th of " + selectedMonth + " " + selectedYear, Toast.LENGTH_LONG).show();
-						
-					}
-					
-				}, year,month,day);
-				dpg.show();
-				
-			}
+				cpg.show(getFragmentManager(), "tag");
+			}//end onClick
 			
 		});
 		return rootView;
 	}
-
 }
